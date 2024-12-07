d1346237377c943ac7e6a4740b3d36bc89546a28
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-12-06 19:10:49 -0500

    add request

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -5,4 +5,5 @@ pub mod query_version_request;
 pub mod query_version_response;
 pub mod read_request;
 pub mod write_request;
+pub mod get_client_shard_info_request;
 pub mod get_client_shard_info_response;
