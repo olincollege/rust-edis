71eee5f6c76260e2028374b77f05c957e02ca49c
Rixhee <rishitbansals@gmail.com>
2024-12-07 14:05:43 -0500

    Added error code in read response

--- src/messages/responses/read_response.rs
+++ src/messages/responses/read_response.rs
@@ -1,13 +1,22 @@
 use crate::messages::message::{MessagePayload, MessageType};
 use anyhow::{Context, Result};
+use int_enum::IntEnum;
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, IntEnum, PartialEq, Eq)]
+pub enum ReadResponseError {
+    NoError = 0,
+    KeyNotFound = 1,
+}
 
 pub struct ReadResponse {
     pub value: Vec<u8>,
+    pub error: u8,
 }
 
 /// Layout of the ReadResponse
-/// | 2 bytes | N bytes |
-/// | valuelen|  value  |
+/// | 1 byte  | 2 bytes | N bytes |
+/// | error   | valuelen|  value  |
 /// Integers are are always encoded in little-endian order
 impl MessagePayload for ReadResponse {
     fn get_message_type(&self) -> MessageType {
@@ -16,6 +25,7 @@ impl MessagePayload for ReadResponse {
 
     fn serialize(&self) -> Result<Vec<u8>> {
         let mut buffer = Vec::new();
+        buffer.push(self.error);
         let value_len = u16::try_from(self.value.len()).context("value length overflow")?;
         buffer.extend_from_slice(&value_len.to_le_bytes());
         buffer.extend_from_slice(&self.value);
@@ -23,16 +33,17 @@ impl MessagePayload for ReadResponse {
     }
 
     fn deserialize(buffer: &[u8]) -> Result<Self> {
+        let error = *buffer.get(0).context("failed to get error")?;
         let value_len = u16::from_le_bytes(
             buffer
-                .get(0..2)
+                .get(1..3)
                 .context("failed to get value length")?
                 .try_into()?,
         );
         let value = buffer
-            .get(2..2 + value_len as usize)
+            .get(3..3 + value_len as usize)
             .context("failed to get value")?
             .to_vec();
-        Ok(ReadResponse { value })
+        Ok(ReadResponse { value, error })
     }
 }
