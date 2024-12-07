6679873b663f9259ccf6051e830e0d6b71678dc9
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-12-06 19:16:18 -0500

    refactor

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -1,9 +1,3 @@
-pub mod get_version_request;
-pub mod get_version_response;
 pub mod message;
-pub mod query_version_request;
-pub mod query_version_response;
-pub mod read_request;
-pub mod write_request;
-pub mod get_client_shard_info_request;
-pub mod get_client_shard_info_response;
+pub mod requests;
+pub mod responses;
---
+++ src/messages/responses/mod.rs
@@ -0,0 +1,3 @@
+pub mod get_client_shard_info_response;
+pub mod query_version_response;
+pub mod get_version_response;
