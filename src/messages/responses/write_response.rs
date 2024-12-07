805c07f59476e67d44f36097d21d4468c9a2d56b
Rixhee <rishitbansals@gmail.com>
2024-12-07 13:42:50 -0500

    Changed write response implem

--- src/messages/responses/write_response.rs
+++ src/messages/responses/write_response.rs
@@ -1,10 +1,21 @@
 use crate::messages::message::{MessagePayload, MessageType};
-use anyhow::Result;
+use anyhow::{Context, Result};
+use int_enum::IntEnum;
 
-pub struct WriteResponse {
-    status: Vec<u8>,
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, IntEnum, PartialEq, Eq)]
+pub enum WriteResponseError {
+    NoError = 0,
+    Error = 1,
 }
 
+pub struct WriteResponse {
+    pub error: u8,
+}
+
+/// Layout of the WriteResponse
+/// | 1 byte |
+/// | error |
 impl MessagePayload for WriteResponse {
     fn get_message_type(&self) -> MessageType {
         MessageType::Write
@@ -12,12 +23,12 @@ impl MessagePayload for WriteResponse {
 
     fn serialize(&self) -> Result<Vec<u8>> {
         let mut buffer = Vec::new();
-        buffer.extend_from_slice(&self.status);
+        buffer.push(self.error);
         Ok(buffer)
     }
 
     fn deserialize(buffer: &[u8]) -> Result<Self> {
-        let status = buffer.to_vec();
-        Ok(WriteResponse { status })
+        let error = *buffer.get(0).context("failed to get error")?;
+        Ok(WriteResponse { error })
     }
 }
