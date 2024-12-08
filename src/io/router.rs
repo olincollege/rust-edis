60ed0322b322af3e6598611588211d5543336f78
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-12-07 19:34:23 -0500

    split with onership

--- src/io/router.rs
+++ src/io/router.rs
@@ -13,11 +13,11 @@ use crate::messages::{requests::{
     write_response::WriteResponse
 }};
 use anyhow::{Result, anyhow, Ok};
-use tokio::{io::{AsyncReadExt, Interest, WriteHalf}, net::{unix::SocketAddr, TcpListener, TcpStream}};
-use std::{cell::RefCell, collections::HashMap};
+use tokio::{io::{AsyncReadExt, Interest}, net::{tcp::{OwnedReadHalf, OwnedWriteHalf, ReadHalf, WriteHalf}, unix::SocketAddr, TcpListener, TcpStream}, task::JoinHandle};
+use std::{cell::RefCell, collections::HashMap, sync::Arc};
 
 
-/// Trait for handling callbacks to inbound/outbound requests
+/// Trait for handling callbacks to requests/responses from peers 
 pub trait RouterHandler {
     /// Callback for handling new requests
     fn handle_announce_shard_request(req: AnnounceShardRequest) -> AnnounceShardResponse;
@@ -30,17 +30,7 @@ pub trait RouterHandler {
 
     fn handle_write_request(req: WriteRequest) -> WriteResponse;
 
-    /// Functions for queueing outbound requests
-    fn queue_announce_shard_request(req: AnnounceShardRequest);
-
-    fn queue_get_client_shard_info_request(req: GetClientShardInfoRequest);
-
-    fn queue_query_version_request(req: QueryVersionRequest);
-
-    fn queue_read_request(req: ReadRequest);
-
-    fn queue_write_request(req: WriteRequest);
-
+  
     /// Callbacks for handling responses to outbound requests
     fn handle_announce_shard_response(res: AnnounceShardResponse);
 
@@ -56,7 +46,9 @@ pub trait RouterHandler {
 
 struct RouterBuilder<T: RouterHandler> {
     handler: T,
-    write_sockets: RefCell<HashMap<String, WriteHalf<TcpStream>>>,
+    /// Map of peer addresses to write sockets
+    write_sockets: RefCell<HashMap<String, Arc<OwnedWriteHalf>>>,
+
 }
 
 impl<T: RouterHandler> RouterBuilder<T> {
@@ -64,20 +56,53 @@ impl<T: RouterHandler> RouterBuilder<T> {
         Self { handler, write_sockets: RefCell::new(HashMap::new()) }
     }
 
-    /// Makes the router start listening for inbound requests
+    /// Functions for queueing outbound requests
+    fn queue_announce_shard_request(req: AnnounceShardRequest, peer: String) {
+
+    }
+
+    fn queue_get_client_shard_info_request(req: GetClientShardInfoRequest, peer: String) {
+
+    }
+  
+    fn queue_query_version_request(req: QueryVersionRequest, peer: String) {
+
+    }
+  
+    fn queue_read_request(req: ReadRequest, peer: String) {
+
+    }
+  
+    fn queue_write_request(req: WriteRequest, peer: String) {
+
+    }
+
+    /// Listens for inbound requests on the read half of a socket from a peer
+    async fn listen_read_half_socket(mut read: OwnedReadHalf) -> Result<()> {
+        let mut buf = [0; 1024];
+        loop {
+            read.readable().await?;
+            read.read(&mut buf).await?;
+            read.peer_addr()?;
+        }
+    }
+
+    /// Makes the router start listening for inbound requests 
     async fn listen(&self) -> Result<()> {
         let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
         loop {
-            let (mut socket, addr) = listener.accept().await?;
-            let (read, write) = tokio::io::split(socket);
+            //let (mut socket, addr) = Arc::new(listener.accept().await?);
+            let (socket, addr) = listener.accept().await?;
+
+            let (read, write) = socket.into_split();
             
             // new peer discovered, add to our list of write sockets
-            self.write_sockets.borrow_mut().insert(addr.to_string(), write);
-
+            self.write_sockets.borrow_mut().insert(addr.to_string(), Arc::new(write));
+            
+            // bind the read half to a background task
             tokio::spawn(async move {
-                let mut buf = [0; 1024];
-                //read.read(&mut buf).await?;
+                Self::listen_read_half_socket(read).await?;
                 Ok(())
             });
         }
