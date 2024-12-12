9310e1d402a973ba0e870ce6136a47de55bbc9fb
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-12-12 17:26:19 -0500

    fix tests

--- src/info.rs
+++ src/info.rs
@@ -1,11 +1,14 @@
 pub mod io;
 pub mod messages;
 
+use std::mem::uninitialized;
+
 use crate::io::router::{RouterBuilder, RouterHandler};
 
 use messages::requests::announce_shard_request::AnnounceShardRequest;
 use messages::requests::get_client_shard_info_request::GetClientShardInfoRequest;
 use messages::requests::get_shared_peers_request::GetSharedPeersRequest;
+use messages::requests::get_version_request::GetVersionRequest;
 use messages::requests::query_version_request::QueryVersionRequest;
 use messages::requests::read_request::ReadRequest;
 use messages::requests::write_request::WriteRequest;
@@ -13,22 +16,20 @@ use messages::requests::write_request::WriteRequest;
 use messages::responses::announce_shard_response::AnnounceShardResponse;
 use messages::responses::get_client_shard_info_response::GetClientShardInfoResponse;
 use messages::responses::get_shared_peers_response::GetSharedPeersResponse;
+use messages::responses::get_version_response::GetVersionResponse;
 use messages::responses::query_version_response::QueryVersionResponse;
 use messages::responses::read_response::ReadResponse;
 use messages::responses::write_response::WriteResponse;
 
+use anyhow::Result;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
-use anyhow::{Result};
 
-struct InfoRouter {
-}
+struct InfoRouter {}
 
 impl InfoRouter {
     pub fn new() -> Self {
-        InfoRouter {
-
-        }
+        InfoRouter {}
     }
 }
 
@@ -41,8 +42,15 @@ impl RouterHandler for InfoRouter {
         unimplemented!()
     }
 
-    fn handle_get_client_shard_info_request(&self, req: &GetClientShardInfoRequest) -> GetClientShardInfoResponse {
+    fn handle_get_client_shard_info_request(
+        &self,
+        req: &GetClientShardInfoRequest,
+    ) -> GetClientShardInfoResponse {
         unimplemented!()
+    }
+
+    fn handle_get_version_request(&self, req: &GetVersionRequest) -> GetVersionResponse {
+        unimplemented!();
     }
 
     /// Info server does not handle requests in relation to the actual key/value state
@@ -50,15 +58,21 @@ impl RouterHandler for InfoRouter {
     fn handle_query_version_request(&self, req: &QueryVersionRequest) -> QueryVersionResponse {
         unimplemented!()
     }
-    fn handle_read_request(&self, req: &ReadRequest) -> ReadResponse {unimplemented!()}
-    fn handle_write_request(&self, req: &WriteRequest) -> WriteResponse {unimplemented!()}
+    fn handle_read_request(&self, req: &ReadRequest) -> ReadResponse {
+        unimplemented!()
+    }
+    fn handle_write_request(&self, req: &WriteRequest) -> WriteResponse {
+        unimplemented!()
+    }
 
-    fn handle_get_shared_peers_request(&self, req: &GetSharedPeersRequest) -> GetSharedPeersResponse {
+    fn handle_get_shared_peers_request(
+        &self,
+        req: &GetSharedPeersRequest,
+    ) -> GetSharedPeersResponse {
         unimplemented!()
     }
 
     /// Callbacks for handling responses to outbound requests
-    
 
     fn handle_get_client_shard_info_response(&self, res: &GetClientShardInfoResponse) {
         unimplemented!()
@@ -68,8 +82,7 @@ impl RouterHandler for InfoRouter {
         unimplemented!()
     }
 
-    fn handle_read_response(&self, res: &ReadResponse) {
-    }
+    fn handle_read_response(&self, res: &ReadResponse) {}
 
     fn handle_write_response(&self, res: &WriteResponse) {
         unimplemented!()
@@ -78,15 +91,19 @@ impl RouterHandler for InfoRouter {
     fn handle_get_shared_peers_response(&self, res: &GetSharedPeersResponse) {
         unimplemented!()
     }
+
+    fn handle_get_version_response(&self, res: &GetVersionResponse) {
+        unimplemented!()
+    }
 }
 
-
-#[tokio::main] 
+#[tokio::main]
 async fn main() -> Result<()> {
     let info_router = InfoRouter::new();
     let info_server = RouterBuilder::new(info_router, None);
     tokio::spawn(async move {
         info_server.listen().await?;
         Ok(())
-    }).await?
+    })
+    .await?
 }
